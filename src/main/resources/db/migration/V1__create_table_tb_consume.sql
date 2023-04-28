7fb298de30da06f6960cf02edb1322313850ab00
allisonvinicius <allisonvinicius63@gmail.com>
2023-04-28 10:45:29 -0400

    feat: create migration whin flyway

---
+++ src/main/resources/db/migration/V1__create_table_tb_consume.sql
@@ -0,0 +1,6 @@
+CREATE TABLE tb_consume(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    carrier VARCHAR(100) NOT NULL,
+    capacity VARCHAR(11) NOT NULL,
+    liters Double NOT NULL
+);
--- src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE tb_consume(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    carrier VARCHAR(100),
-    capacity VARCHAR(11),
-    liters Double 
-);
